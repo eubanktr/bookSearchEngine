76a0f31336697f71b44ddc5d0ed1dad722379eda
Thomas Eubank <eubanktr@gmail.com>
2021-09-13 22:12:40 -0400

    got dependecies working started on typeDefs and resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,20 @@
+const { Book, User } = require('../models');
+
+const resolvers = {
+  Query: {
+    book: async () => {
+      return Book.find({})
+    },
+    user: async (parent, {_id}) =>{
+      const params = _id ? { _id } : {};
+      return User.find(params);
+    },
+  },
+  Mutation: {
+    createUser: async (parent, args) => {
+      const user = await User.create(args);
+      return user;
+    },
+    
+  }
+}
